36538114e86a25d4cf2b6fbfd3bb5ddb0630a4af
Anders Wallin <anders.e.e.wallin@gmail.com>
2017-01-07 20:41:00 +0200

    utc time for date

--- digiclock.py
+++ digiclock.py
@@ -6,7 +6,7 @@
 # Anders E. Wallin, 2015-04-24
 # GPLv3 license.
 #
-# added: UTC, localtime, date, MJD, DOY, week 
+# added: UTC, localtime, date, MJD, DOY, week
 from Tkinter import *
 import time
 import jdutil # https://gist.github.com/jiffyclub/1294443
@@ -36,10 +36,10 @@ def tick():
     time2 = time.strftime('%H:%M:%S') # local
     time_utc = time.strftime('%H:%M:%S', time.gmtime()) # utc
     # MJD
-    date_iso_txt = time.strftime('%Y-%m-%d') + "    %.5f" % jdutil.mjd_now()
+    date_iso_txt = time.strftime('%Y-%m-%d', time.gmtime()) + "    %.5f" % jdutil.mjd_now()
     # day, DOY, week
     date_etc_txt = "%s   DOY: %s  Week: %s" % (time.strftime('%A'), time.strftime('%j'), time.strftime('%W'))
-    
+
     if time2 != time1: # if time string has changed, update it
         time1 = time2
         clock_lt.config(text=time2)
@@ -50,6 +50,6 @@ def tick():
     # to update the time display as needed
     # could use >200 ms, but display gets jerky
     clock_lt.after(20, tick)
-    
+
 tick()
 root.mainloop()
